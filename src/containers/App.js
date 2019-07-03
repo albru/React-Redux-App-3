0ca15f8dcd7f78d38191f413b17a27727bbad227
albru <bryukhanovspb@gmail.com>
2019-07-03 16:32:29 +0300

    starting implimentation of the burger builder ccontainer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,130 +1,21 @@
 import React, { Component } from 'react';
-import classes from './App.css';
-import Persons from './../components/Persons/Persons';
-import Cockpit from './../components/Cockpit/Cockpit';
-import withClass from '../hoc/withClass';
 import Aux from '../hoc/_Aux';
-import AuthContext from '../context/auth-context';
+import Layout from '../components/Layout/Layout';
+import BurgerBuilder from '../containers/BurgerBuilder/BurgerBuilder';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    console.log('[App.js] constructor')
-  }
-
-  state = {
-    persons: [
-      { id: 'grcadfc3', name: 'Max', age: 28 },
-      { id: 'axd32x',  name: 'Manu', age: 29 },
-      { id: 'nsd223s1',  name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false,
-    showCockpit: true,
-    changeCounter: 0,
-    authenticated: false
-  }
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
-  }
-  // componentWillMount() {
-  //   console.log('[App.js] componentWillMount')
-  // }
-
-  componentDidMount() {
-    console.log('[App.js] componentDidMount');
-  }
-
-  shouldComponentUpdate() {
-    console.log('[App.js] shouldComponentUpdate');
-    return true;
-  }
-
-  componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate');
-  }
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
-
-  nameChangedHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-    // const person = Object.assign({}, this.state.persons[perosIndex])
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-    this.setState((prevState, props) => {
-      return {
-        persons: persons, 
-        changeCounter: prevState.changeCounter + 1 
-      };
-    }) ;
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  };
-
-  loginHandler = () => {
-    this.setState({authenticated: true});
-  };
 
   render () {
-    console.log('[App.js] render');
-
-    let persons = null; 
-    if (this.state.showPersons) {
-      persons = <Persons 
-          personsLength={this.state.persons.length}
-          clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler}
-          persons={this.state.persons}
-          isAuthenticated={this.state.authenticated}
-          />
-    }
-
     return (
       <Aux>
-        <button 
-        onClick={() => {
-            this.setState({showCockpit:false})
-          }}>
-          Remove Cockpit
-        </button>
-        <AuthContext.Provider 
-          value={{
-            authenticated: this.state.authenticated, 
-            login: this.loginHandler
-          }}>
-          {this.state.showCockpit ? (
-          <Cockpit 
-            title={this.props.appTitle}
-            showPersons={this.state.showPersons}
-            persons={this.state.persons}
-            clicked={this.togglePersonsHandler}
-          /> 
-          ) : null}
-          {persons}
-        </AuthContext.Provider>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default withClass(App, classes.App);
+export default App;
