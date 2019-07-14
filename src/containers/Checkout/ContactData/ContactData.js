84ba79f517d3ee54ad8305b48e771f7681773364
albru <bryukhanovspb@gmail.com>
2019-07-14 20:29:09 +0300

    accessing protected resouces

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,7 @@ class ContactData extends Component {
         price: finalPrice.toFixed(2),
         orderData: formData
     }
-    this.props.onOrderBurger(order)
+    this.props.onOrderBurger(order, this.props.token)
   }
 
   checkValidity(value, rules) {
@@ -186,14 +186,15 @@ const mapStetToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
   }
 };
 
 const mapDispatchToProps = dispatch => {
 
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
   };
   
 }
