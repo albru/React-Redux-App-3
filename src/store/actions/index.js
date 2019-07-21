b687bd9997ade505ea8bf0e79002215bbb1af03c
albru <bryukhanovspb@gmail.com>
2019-07-21 21:13:43 +0300

    moving the burger builder side effects into a saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,23 @@
-export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
-export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, authFail, checkAuthTimeout } from './auth';
+export {
+    addIngredient,
+    removeIngredient,
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
+} from './burgerBuilder';
+export {
+    purchaseBurger,
+    purchaseInit,
+    fetchOrders
+} from './order';
+export {
+    auth,
+    logout,
+    setAuthRedirectPath,
+    authCheckState,
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    authFail,
+    checkAuthTimeout
+} from './auth';
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,16 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionsTypes';
-import { logoutSaga, cheackAuthTimeoutSaga, authUserSaga } from './auth';
+import { logoutSaga, cheackAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
+import { initIngredientsSaga } from './burgerBuilder';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, cheackAuthTimeoutSaga);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
 }
