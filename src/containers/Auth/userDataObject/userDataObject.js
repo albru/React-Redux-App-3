344f06f971f28703d80863a0d8a5629eb9f289d1
albru <bryukhanovspb@gmail.com>
2019-07-23 22:32:51 +0300

    adjusting Auth & Logout components

---
+++ src/containers/Auth/userDataObject/userDataObject.js
@@ -0,0 +1,32 @@
+export const userDataObject = (() => {
+    return {
+        email: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'email',
+              placeholder: 'Mail Address'
+            },
+            value: '',
+            validation: {
+              required: true,
+              isEmail: true
+            },
+            valid: false,
+            touched: false
+          },
+          password: {
+            elementType: 'input',
+            elementConfig: {
+              type: 'password',
+              placeholder: 'Password'
+            },
+            value: '',
+            validation: {
+              required: true,
+              minLength: 6
+            },
+            valid: false,
+            touched: false
+          }
+        }
+});
